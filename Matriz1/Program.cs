86404bd6091966d6a18b96607bfa6052282ae3ae
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-11-18 22:01:00 -0300

    Adicionar arquivos de projeto.

---
+++ Matriz1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Matriz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a ordem da matriz: "); // Definir a ordem da matriz
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+            for (int i = 0; i < n; i++) // Colocar os valores dentro da matriz
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Valor [" + i + ", " + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            int negative = 0;
+            Console.Write("A diagonal principal é: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j) // Imprime os valores da diagonal
+                    {
+                        Console.Write(mat[i, j] + " ");
+                    }
+                    if (mat[i, j] < 0) // Imprime valores negativos
+                    {
+                        negative++;
+                    }
+                }
+            }
+            Console.Write("\nOs números Negativos são: " + negative);
+            Console.ReadKey();
+        }
+    }
+}
